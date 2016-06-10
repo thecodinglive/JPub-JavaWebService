d3bf31ead083ecb4af11390f1289bc14ded8cac4
sjyun <darkandark90@gmail.com>
2016-06-10 18:18:41 +0900

    remove

--- ch10/src/test/java/TestMQConfig.java
+++
@@ -1,5 +0,0 @@
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.config.AbstractRabbitConfiguration;
-
-public class TestMQConfig extends AbstractRabbitConfiguration{
-}
