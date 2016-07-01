45bbb1f01228c2847f0bfa23295c3d851d54fe61
sjyun <darkandark90@gmail.com>
2016-07-01 18:45:42 +0900

    rest basic

--- ch05/src/main/java/info/thecodinglive/model/Todo.java
+++ ch05/src/main/java/info/thecodinglive/model/Todo.java
@@ -8,4 +8,20 @@ public class Todo {
         this.id = id;
         this.title = title;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
