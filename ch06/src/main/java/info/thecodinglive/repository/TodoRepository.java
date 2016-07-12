5444f8a8f148ef0b209cbf8cc53fba2f64816657
sjyun <darkandark90@gmail.com>
2016-07-12 19:59:57 +0900

    todo

--- ch06/src/main/java/info/thecodinglive/repository/TodoRepository.java
+++ ch06/src/main/java/info/thecodinglive/repository/TodoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-@Repository
+
 public interface TodoRepository extends JpaRepository<Todo,Long> {
 }
