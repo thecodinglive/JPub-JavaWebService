9cb1f002ca34738402d7fff68eaf902be44a040b
sjyun <darkandark90@gmail.com>
2016-08-12 16:50:32 +0900

    ch08

---
+++ ch08/src/main/java/info/thecodinglive/SecurityApp.java
@@ -0,0 +1,12 @@
+package info.thecodinglive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SecurityApp {
+    public static void main(String ar[]){
+        SpringApplication.run(SecurityApp.class, ar);
+    }
+}
