cfb51f52b998d66e34b9d9cf26e498a3389be9bd
윤석진 <sukjin-yun@titanplatform.net>
2017-01-08 08:08:42 +0900

    mybatisConfig

--- ch06-mybatis/src/main/java/info/thecodinglive/config/MyBatisConfig.java
+++ ch06-mybatis/src/main/java/info/thecodinglive/config/MyBatisConfig.java
@@ -3,6 +3,7 @@ package info.thecodinglive.config;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -13,6 +14,7 @@ import javax.sql.DataSource;
  * Created by yun_dev1 on 2017-01-06.
  */
 @Configuration
+@MapperScan(basePackages = {"info.thecodinglive.repository"})
 public class MyBatisConfig {
 
     @Bean
@@ -24,6 +26,7 @@ public class MyBatisConfig {
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setConfigLocation((new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml")));
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:sample/mapper/*.xml"));
         return sqlSessionFactoryBean.getObject();
     }
--- ch06-mybatis/src/main/java/info/thecodinglive/repository/UserRepository.java
+++ ch06-mybatis/src/main/java/info/thecodinglive/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package info.thecodinglive.repository;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+        import org.mybatis.spring.SqlSessionTemplate;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Repository;
 
-import java.util.List;
+        import java.util.List;
 
 /**
  * Created by yun_dev1 on 2017-01-06.
