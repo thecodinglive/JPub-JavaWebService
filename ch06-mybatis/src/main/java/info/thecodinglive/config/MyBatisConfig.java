22ae9c9617d81ae5503a524803dcb67ac23d836a
sjyun <darkandark90@gmail.com>
2017-01-15 19:01:54 +0900

    dataSourcebean add

--- ch06-mybatis/src/main/java/info/thecodinglive/config/MyBatisConfig.java
+++ ch06-mybatis/src/main/java/info/thecodinglive/config/MyBatisConfig.java
@@ -3,11 +3,12 @@ package info.thecodinglive.config;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
@@ -19,14 +20,24 @@ import javax.sql.DataSource;
 public class MyBatisConfig {
 
     @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                    .setName("jpubtestdb")
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("schema-hsqldb.sql")
+                .addScript("data-hsqldb.sql")
+                .build();
+    }
+
+    @Bean
     public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 
     @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+    public SqlSessionFactory sqlSessionFactory() throws Exception{
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setDataSource(dataSource());
         sqlSessionFactoryBean.setConfigLocation((new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml")));
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:sample/mapper/*.xml"));
         return sqlSessionFactoryBean.getObject();
