b9ba7f5d6648e789a3e1f1a8be1861014c82c8a4
sukjin.yun <sukjin.yun@navercorp.com>
2017-11-24 18:05:45 +0900

    bean config설정 추가

--- ch03/src/main/java/info/thecodinglive/javaConfig/ConfigImportSpringApp.java
+++ ch03/src/main/java/info/thecodinglive/javaConfig/ConfigImportSpringApp.java
@@ -1,12 +1,15 @@
 package info.thecodinglive.javaConfig;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class JavaConfigSpringApp {
-    public static void main(String ar[]){
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(BeanConfig.class);
-        context.refresh();
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Import;
+
+@Import(BeanConfig.class)
+public class ConfigImportSpringApp {
+    public static void main(String ar[]) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                BeanConfig.class
+        );
 
         WorkService yourWorkService = context.getBean("yourWorkService", WorkService.class);
         yourWorkService.askWork();
---
+++ ch03/src/main/java/info/thecodinglive/javaConfig/WorkManager.java
@@ -0,0 +1,5 @@
+package info.thecodinglive.javaConfig;
+
+public interface WorkManager {
+    public String doIt();
+}
